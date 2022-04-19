3c28e1bd7a7741a8c39e18c948836078d208c408
Nathan <nathanpulkis@gmail.com>
2022-04-19 20:26:41 +0100

    Added social media share buttons

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="w-4/5 m-auto text-left">
     <div class="py-15">
         <h1 class="text-6xl">
@@ -16,7 +17,23 @@
 
     <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
         {{ $post->description }}
+
     </p>
+
+    <div class="container mt-4">
+        <h6>Share this blogpost!</h6>
+        {!! $share = \Share::page($currentURL = url()->current(),'Check out this blog post')->facebook()->twitter()->reddit()->whatsapp();
+!!}
+    </div>
+
+
+
 </div>
 
-@endsection 
+
+@endsection
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
+<script src="{{ asset('js/share.js') }}"></script>
